9cb55174d6ee0e3809f51bada9424565976381c5
Amanda Hotchkiss <77412748+hotkiss1011@users.noreply.github.com>
2022-04-13 16:30:54 -0400

    Commented out code

--- src/index.js
+++ src/index.js
@@ -5,28 +5,34 @@ sketchArea.setAttribute('style', 'display: flex; flex-direction: column; border-
 let colorBtn = document.querySelector('.color-btn');
 let color = document.querySelector('#color');
 
-//set color
+//set start color
 let colorInput = color.value;
 colorBtn.setAttribute('style', `background-color: ${colorInput}`);
 
+//on color button click, click color input
 colorBtn.addEventListener('click', function() {
     color.click();
 });
 
+//on change of color value, update color input value and button background color
 color.addEventListener('change', function() {
     colorInput = color.value;
     colorBtn.setAttribute('style', `background-color: ${colorInput}`);
+    return colorInput;
 })
 
-
+//set number of row/columns - may change this later
 let n = 16;
 
+//makes grid that is n squares wide and n squares tall
 function makeGrid(n) {
+    //this makes each of the rows
     for (let j=0; j < n; j++) {
         let row = document.createElement('div');
         row.classList.add('row');
         row.setAttribute('style', 'display: flex;')
         
+        //this creates each square and puts it in a row
         for (let i=0; i < n; i++) {
             let square = document.createElement('div');
             square.classList.add(`square`);
@@ -38,4 +44,5 @@ function makeGrid(n) {
     }
 }
 
+//creates grid on load
 makeGrid(n);
